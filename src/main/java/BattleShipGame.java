6c0d46510d3689baf1e57830e47b4dc71fb5d6b1
Nilofar Nisa H <nilofarnisa28@gmail.com>
2021-03-23 15:55:52 +0530

    [Nisa / Sameera] Add. functionality to quit the game when the player decides to quit

--- src/main/java/BattleShipGame.java
+++ src/main/java/BattleShipGame.java
@@ -48,18 +48,27 @@ public class BattleShipGame {
             }
         }
 
-       /*for (int i = 0; i < board.length; i++) {
+       for (int i = 0; i < board.length; i++) {
             System.out.print("| ");
             for (int j = 0; j < board[i].length; j++) {
                 System.out.print(board[i][j]+" ");
             }
             System.out.println();
-        }*/
+        }
     }
 
     public static String shootShip(int xCoordinate, int yCoordinate) {
+        if(board[xCoordinate][yCoordinate] == "X" || board[xCoordinate][yCoordinate] == "*")
+        {
+            return "Shot Already , Choose other co-ordinates";
+        }
         if(hit(xCoordinate,yCoordinate))
         {
+            noOfShips--;
+            if(noOfShips == 0)
+            {
+                return "You Won :)";
+            }
             return "HIT";
         }
         return "MISS";
@@ -72,19 +81,51 @@ public class BattleShipGame {
             playerBoard[xCoordinate][yCoordinate] = "X";
             return true;
         }
+        board[xCoordinate][yCoordinate] = "*";
         playerBoard[xCoordinate][yCoordinate] = "*";
         return false;
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Setting the board....");
         setBoard();
+        System.out.println("Board Set");
         printBoard();
+        System.out.println("Setting the Ships in positions");
         setShip();
-        int x = input.nextInt();
-        int y = input.nextInt();
-        String result = shootShip(x, y);
-        System.out.println(result);
-        printBoard();
+        int choice;
+        do {
+            System.out.println("Choose your option : 1.SHOOT 2.PRINT BOARD 3.QUIT GAME");
+            choice = input.nextInt();
+            switch(choice)
+            {
+                case 1:
+                    System.out.println("SHOOT");
+                    System.out.println("Enter X and Y co-ordinates: ");
+                    int x = input.nextInt();
+                    int y = input.nextInt();
+                    if((x>=0 && x<=9) && (y>=0 && y<=9) )
+                    {
+                        String result = shootShip(x, y);
+                        System.out.println(result);
+                        if(result == "You Won :)")
+                            return;
+                        //printBoard();
+                    }
+                    System.out.println("Co-ordinates out of Range. Please enter any value from 0 to 9");
+                    break;
+                case 2:
+                    System.out.println("PRINT BOARD :");
+                    printBoard();
+                    break;
+                case 3:
+                    System.out.println("You Lost :(");
+                    break;
+                default :
+                    System.out.println("Wrong choice . Choose correct option");
+                    break;
+            }
+        }while(choice!=3);
     }
 }
